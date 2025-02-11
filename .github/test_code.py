76201cf13ff66e2dc2d3871f9ebe47fe7141439a
prasad2309 <psychemission@gmail.com>
2025-02-10 19:00:06 -0700

    Adding test file

---
+++ .github/test_code.py
@@ -0,0 +1,22 @@
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    result = a * b
+    # Unused variable
+    temp = a + b
+    return result
+
+def divide_numbers(a, b):
+    # Potential bug: Division by zero
+    return a / b
+
+# Incorrect indentation
+def faulty_function():
+x = 10
+    print(x)
+
+# Calling functions
+print(add_numbers(5, 10))
+print(multiply_numbers(3, 4))
+print(divide_numbers(8, 0))
